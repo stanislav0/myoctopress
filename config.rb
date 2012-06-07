0f9ae2d0f88c3cef8fe47bf1405f7bbda49724a4
stanislav0 <stanislav.stoyanov.bg@gmail.com>
2012-06-07 16:20:04 +0300

    init

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/lapiovra/"
+http_images_path = "/lapiovra/images"
+http_fonts_path = "/lapiovra/fonts"
+css_dir = "public/lapiovra/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
